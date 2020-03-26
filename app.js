44f6a2385cc21397afe5801e54293287570ab4c5
Chris Creusat <ch.cst@laposte.net>
2020-03-26 12:08:42 +0100

    Optimisation du code

--- app.js
+++ app.js
@@ -2,30 +2,53 @@ $(document).ready(() => {
     ChessBoard();
 });
 
+// Mieux opti
+
 function ChessBoard() {
+    
     let blackBoard = ["Tour", "Cavalier", "Fou", "Reine", "Roi", "Fou", "Cavalier", "Tour", "Pion", "Pion", "Pion", "Pion","Pion", "Pion","Pion", "Pion"];
     let whiteBoard = ["Tour", "Cavalier", "Fou", "Roi", "Reine", "Fou", "Cavalier", "Tour", "Pion", "Pion","Pion", "Pion","Pion", "Pion","Pion", "Pion"];
     let caseClass = "blanc";
 
-    function showCase(nb, color, board) {
-        switch(color) {
-            case "NOIR" : rep = board[nb] + " NOIR"; break;
-            case "BLANC" : rep = board[nb] + " BLANC"; break;
-        }
-        return rep;
-    }
-
     for(let i = 1; i <= 64; i++) {
         element = document.createElement('div');
         element.className = caseClass + " case";
-        if(i <= 16)
-            element.innerHTML = showCase(i-1, "NOIR", blackBoard)
-        if(i > 48)
-            element.innerHTML = showCase(64-i, "BLANC", whiteBoard)
-        if(i % 8 != 0)
-            caseClass == "blanc" ?
-            caseClass = "noir" :
-            caseClass = "blanc";
+        element.innerHTML = (i <= 16) ? blackBoard[i-1] + " NOIR" : (i > 48) ? whiteBoard[64-i] + " BLANC" : "";
+        if(i % 8 != 0) {
+            caseClass == "blanc" ? caseClass = "noir" : caseClass = "blanc";
+        } 
         document.getElementById("jeu").appendChild(element);
     }
 }
+
+// Méthode pas opti
+
+// function ChessBoard() {
+
+//     let blackBoard = ["Tour", "Cavalier", "Fou", "Reine", "Roi", "Fou", "Cavalier", "Tour", "Pion", "Pion", "Pion", "Pion","Pion", "Pion","Pion", "Pion"];
+//     let whiteBoard = ["Tour", "Cavalier", "Fou", "Roi", "Reine", "Fou", "Cavalier", "Tour", "Pion", "Pion","Pion", "Pion","Pion", "Pion","Pion", "Pion"];
+//     let caseClass = "blanc";
+
+//     function showCase(nb, color, board) {
+//         switch(color) {
+//             case "NOIR" : rep = board[nb] + " NOIR"; break;
+//             case "BLANC" : rep = board[nb] + " BLANC"; break;
+//         }
+//         return rep;
+//     }
+
+//     for(let i = 1; i <= 64; i++) {
+//         element = document.createElement('div');
+//         element.className = caseClass + " case";
+//         if(i <= 16)
+//             element.innerHTML = showCase(i-1, "NOIR", blackBoard)
+//         if(i > 48)
+//             element.innerHTML = showCase(64-i, "BLANC", whiteBoard)
+//         if(i % 8 != 0)
+//             caseClass == "blanc" ?
+//             caseClass = "noir" :
+//             caseClass = "blanc";
+//         document.getElementById("jeu").appendChild(element);
+//     }
+// }
+
