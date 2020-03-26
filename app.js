efbcb5f42894c8ccc298b4a6d480d5c99a3cd173
Chris Creusat <ch.cst@laposte.net>
2020-03-26 12:26:05 +0100

    Adding commentaries

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
+// WAITING MY DOCUMENT TO BE READY BEFORE SETTING UP MY CHESSBOARD
 $(document).ready(() => {
     ChessBoard();
 });
 
-// Mieux opti
+// MORE OPTIMIZED
 
 function ChessBoard() {
-    
+    // SETTING THINGS I NEED LIKE CHESS PIECES AND THE STARTING BACKGROUND COLOR
     let blackBoard = ["Tour", "Cavalier", "Fou", "Reine", "Roi", "Fou", "Cavalier", "Tour", "Pion", "Pion", "Pion", "Pion","Pion", "Pion","Pion", "Pion"];
     let whiteBoard = ["Tour", "Cavalier", "Fou", "Roi", "Reine", "Fou", "Cavalier", "Tour", "Pion", "Pion","Pion", "Pion","Pion", "Pion","Pion", "Pion"];
     let caseClass = "blanc";
 
     for(let i = 1; i <= 64; i++) {
+        // CREATING THE DIV
         element = document.createElement('div');
+        // ADDING HER CLASS + MY CSS CLASSES NEEDED
         element.className = caseClass + " case";
+        // FOR EACH DIV I MAKE, BY THE VALUE OF "i" I KNOW WHAT CHESS PIECE I NEED TO ADD IN THE TAG
         element.innerHTML = (i <= 16) ? blackBoard[i-1] + " NOIR" : (i > 48) ? whiteBoard[64-i] + " BLANC" : "";
+        // EVERY SINGLE ROW, I SWITCH BACKGROUND COLOR CLASSES SO I GET A PERFECT CHESSBOARD SETUP
         if(i % 8 != 0) {
             caseClass == "blanc" ? caseClass = "noir" : caseClass = "blanc";
-        } 
+        }
+        // AND THEN, I ADD MY CREATED DIV WITH ALL SPECS IN MY DOCUMENT
         document.getElementById("jeu").appendChild(element);
     }
 }
 
-// Méthode pas opti
+// LESS OPTIMIZED
 
 // function ChessBoard() {
 
